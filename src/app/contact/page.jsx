89412cc2bef356b4f8ab4fd2c472ffad6f5c0d48
Saif <ayeshsaif367@gmail.com>
2023-10-05 07:24:24 +0300

    finished contact page for desktop size

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,56 @@
+'use client';
+import Link from 'next/link';
 import styles from '../style/contact.module.css';
 
 function page() {
   return (
-    <div>
-        Contact
+    <div className={styles.container}>
+      <div>
+        <div>
+          <h1>Contact Information</h1>
+          <br />
+          <p>Here you can find all communication methods to contact with us.</p>
+        </div>
+        <div>
+          <h3>Email: saifchan@mail.com</h3>
+          <br />
+          <h3>Phone No. : +962 799 562 745</h3>
+        </div>
+        <div className={styles.accounts}>
+          <h2>Our Accounts :</h2>
+          <ul>
+            <li>
+              <Link href={'#'}>
+                <h3>account one</h3>
+              </Link>
+            </li>
+            <li>
+              <Link href={'#'}>
+                <h3>account two</h3>
+              </Link>
+            </li>
+            <li>
+              <Link href={'#'}>
+                <h3>account three</h3>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.form}>
+          <h2>
+            If there is any problem, or you want to ask about something, 
+            you can send us a message:
+          </h2>
+          <br />
+          <form action="">
+            <input type="email" name="" id="" placeholder='email address' />
+            <input type="text" name="" id="" placeholder='subject' />
+            <textarea name="" id="" placeholder='message content' ></textarea>
+            <button type="submit">send</button>
+          </form>
+
+        </div>
+      </div>
     </div>
   )
 }
