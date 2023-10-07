bb9f195bcca77e83d68632111061edcdcda09280
Saif <ayeshsaif367@gmail.com>
2023-10-07 19:59:20 +0300

    portfolio link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -170,7 +170,7 @@ export default function Home() {
         </section>
           
         <section className={styles.work}>
-          <Link href={'/portolio'}>
+          <Link href={'/portfolio'}>
             <h2>See some samples of our work and build trust.</h2>
             <svg xmlns="http://www.w3.org/2000/svg" width="146" height="30" viewBox="0 0 146 30" fill="none">
               <path d="M145.414 16.4142C146.195 15.6332 146.195 14.3668 145.414 13.5858L132.686 0.857864C131.905 0.0768156 130.639 0.0768156 129.858 0.857864C129.077 1.63891 129.077 2.90524 129.858 3.68629L141.172 15L129.858 26.3137C129.077 27.0948 129.077 28.3611 129.858 29.1421C130.639 29.9232 131.905 29.9232 132.686 29.1421L145.414 16.4142ZM0 17H144V13H0V17Z" fill="black"/>
